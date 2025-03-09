549846af004a7d0c3dff54241e78e94ee8b0f9f7
BOT-K4CP3R <kontakt.n4rmoo@gmail.com>
2025-03-09 21:57:24 +0100

    add expires_in property to TokenResponse interface and update package version to 1.0.5

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -3,6 +3,7 @@ export interface TokenResponse {
     token_type: string;
     refresh_token: string;
     expiry: string;
+    expires_in: number;
     scope: string;
 }
 export interface KickAuthConfig {
